584498d09f6af6cc0a8393e40d4a34b3d266492f
divyesh006 <divyesh10008@gmail.com>
2022-12-21 18:28:59 +0530

    String

---
+++ Robot Return to Origin/src/Main.java
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/robot-return-to-origin/
+public class Main {
+    c
+    public static void main(String[] args) {
+        String moves = "UD";
+        System.out.println(judgeCircle(moves));
+    }
+}
